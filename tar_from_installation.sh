f977ea861e2b8c35823780de21a09c29c419a9c5
Sam Clegg <sbc@chromium.org>
2019-10-31 11:00:46 -0700

    Create standalone archive of the wasi sysroot
    Fixes: #69

--- tar_from_installation.sh
+++ tar_from_installation.sh
@@ -20,4 +20,9 @@ rm -rf $PKGDIR
 cp -R /opt/wasi-sdk $PKGDIR
 cd build
 tar czf wasi-sdk-$VERSION\-$MACHINE.tar.gz wasi-sdk-$VERSION
+
+# As well as the full SDK package, also create archives of libclang_rt.builtins
+# and the sysroot. These are made available for users who have an existing clang
+# installation.
 tar czf libclang_rt.builtins-wasm32-wasi-$VERSION.tar.gz -C compiler-rt lib/wasi
+tar czf wasi-sysroot-$VERSION.tar.gz -C wasi-sdk-$VERSION/share wasi-sysroot
