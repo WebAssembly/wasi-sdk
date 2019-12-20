757c2627193d4d793b30f39e1db8e06a16be255c
Dan Gohman <sunfish@mozilla.com>
2019-12-20 14:39:21 -0800

    Add a way to run the testsuite in compile-only mode.

--- tests/run.sh
+++ tests/run.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 set -ueo pipefail
 
-# Top-level test runner. Usage is "run.sh <runwasi>" where <runwasi> is a
-# WASI-capable runtime.
+# Top-level test runner. Usage is "run.sh" to run tests in compile-only mode,
+# or "run.sh <runwasi>" where <runwasi> is a WASI-capable runtime to run the
+# tests in full compile and execute mode.
 
-runwasi="$1"
+# Determine the wasm runtime to use, if one is provided.
+if [ $# -gt 0 ]; then
+    runwasi="$1"
+else
+    runwasi=""
+fi
 
 cd compile-only
 for options in -O0 -O2 "-O2 -flto"; do
     echo "===== Testing compile-only with $options ====="
     for file in *.c; do
         echo "Testing compile-only $file..."
-        ../testcase.sh true clang "$options" "$file"
+        ../testcase.sh "" clang "$options" "$file"
     done
     for file in *.cc; do
         echo "Testing compile-only $file..."
-        ../testcase.sh true clang++ "$options" "$file"
+        ../testcase.sh "" clang++ "$options" "$file"
     done
 done
 cd - >/dev/null
