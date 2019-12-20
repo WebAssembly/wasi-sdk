4e98b07c4e2382393ca7b5bc8d2ce995b0d8cfa7
Dan Gohman <sunfish@mozilla.com>
2019-12-20 14:13:30 -0800

    Rename runwasm to runwasi.

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ueo pipefail
 
-runwasm="$1"
+runwasi="$1"
 
 cd compile-only
 for options in -O0 -O2 "-O2 -flto"; do
@@ -22,11 +22,11 @@ for options in -O0 -O2 "-O2 -flto"; do
     echo "===== Testing with $options ====="
     for file in *.c; do
         echo "Testing $file..."
-        ../testcase.sh "$runwasm" clang "$options" "$file"
+        ../testcase.sh "$runwasi" clang "$options" "$file"
     done
     for file in *.cc; do
         echo "Testing $file..."
-        ../testcase.sh "$runwasm" clang++ "$options" "$file"
+        ../testcase.sh "$runwasi" clang++ "$options" "$file"
     done
 done
 cd - >/dev/null
