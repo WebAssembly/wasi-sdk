cd337c9418916ed453d57620e29d6b0e101ba182
Pat Hickey <pat@moreproductive.org>
2020-04-08 12:42:21 -0700

    fix typo in deb creation

--- deb_from_installation.sh
+++ deb_from_installation.sh
@@ -18,4 +18,4 @@ mkdir -p build/pkg/DEBIAN
 sed -e s/VERSION/$VERSION/ wasi-sdk.control > build/pkg/DEBIAN/control
 cp -R /opt/wasi-sdk build/pkg/opt/
 cd build && dpkg-deb -b pkg wasi-sdk_$VERSION\_amd64.deb && cd ..
-mv build/wasi-sdk_$VERSION\amd64.deb $OUTDIR/
+mv build/wasi-sdk_$VERSION\_amd64.deb $OUTDIR/
