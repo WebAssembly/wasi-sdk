cea7ecbdc52cbac9dcb03b6d35c9c5f3c5e37b6e
Dan Gohman <sunfish@mozilla.com>
2020-05-29 11:13:54 -0700

    Add a testcase for raise(SIGABRT).

---
+++ tests/general/sigabrt.c
@@ -0,0 +1,11 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    fprintf("raising SIGABRT...\n");
+    raise(SIGABRT);
+    fprintf("oops!\n");
+    return EXIT_FAILURE;
+}
