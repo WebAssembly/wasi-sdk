2d0ecd300fd7de6d81738d5005ecbc275a267c5c
Yonggang Luo <luoyonggang@gmail.com>
2022-04-23 04:32:37 +0800

    Fixes  docker build on Linux
    Fixes #230
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 set -e
+echo "Building the docker"
 docker build -t wasi-sdk-builder:latest .
+echo "Building the package in docker"
 docker run --mount type=bind,src=$PWD,target=/workspace -e NINJA_FLAGS=-v --workdir /workspace wasi-sdk-builder:latest make package
