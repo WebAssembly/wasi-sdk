96bbd95197cf24cf070a04aa4a7bbc6115ae5df3
Mike Hommey <mh@glandium.org>
2024-03-13 08:37:09 +0900

    Support getting LLVM version from its new location (#399)
    https://github.com/llvm/llvm-project/pull/84641 changed its location.

--- llvm_version_major.sh
+++ llvm_version_major.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 LLVM_PROJ_DIR=${1:-./src/llvm-project}
-MAJOR=`grep "set(LLVM_VERSION_MAJOR" $LLVM_PROJ_DIR/llvm/CMakeLists.txt | awk '{print substr($2, 1, length($2) - 1)}'`
+MAJOR=`(grep "set(LLVM_VERSION_MAJOR" $LLVM_PROJ_DIR/llvm/CMakeLists.txt || grep "set(LLVM_VERSION_MAJOR" $LLVM_PROJ_DIR/cmake/Modules/LLVMVersion.cmake) | awk '{print substr($2, 1, length($2) - 1)}'`
 echo $MAJOR
