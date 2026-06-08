5faf80805397ae2a96ab224d1f103798af06dd92
Alex Crichton <alex@alexcrichton.com>
2026-06-08 16:30:00 -0500

    Set `CMAKE_EXECUTABLE_SUFFIX` in toolchain files (#634)
    Closes #633

--- wasi-sdk-pthread.cmake
+++ wasi-sdk-pthread.cmake
@@ -11,6 +11,7 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
 # (--export-memory is implicit unless --import-memory is given)
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--import-memory")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--export-memory")
+set(CMAKE_EXECUTABLE_SUFFIX .wasm)
 
 if(WIN32)
 	set(WASI_HOST_EXE_SUFFIX ".exe")
